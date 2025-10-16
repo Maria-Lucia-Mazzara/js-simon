f6b7ec4bbee55c14747b7879423323f6ea162f84
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-16 16:21:36 +0200

    collegato le classi a js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.dir(document);
+
+const time = document.getElementById("countdown");
+const istruzione = document.getElementById("instructions");
+const numbers = document.getElementById("numbers-list");
+const myForm = document.getElementById("answers-form");
+const btn = document.querySelector("btn");
+const risultato_html = document.getElementById("risultato");
+
